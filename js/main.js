c47cd14e54be6680ecb1e9e3df196a432c641bf9
Nicolas Inden <nicolas.inden@rwth-aachen.de>
2013-06-18 10:23:01 +0200

    Map zooms correctly when adding waypoint + we can create paths

--- js/main.js
+++ js/main.js
@@ -14,31 +14,66 @@ $(function () {
   }
   
   function showPosition(position) {
+    window.waypoints = new Array();
+    window.wpNo = 0;
+    var radius = position.coords.accuracy / 2;
+    var latlng = new L.LatLng(position.coords.latitude,position.coords.longitude);
+    window.waypoints.push(latlng);
+
     console.log("Latitude: " + position.coords.latitude +
-    "\nLongitude: " + position.coords.longitude);
+    "\nLongitude: " + position.coords.longitude +
+    "\nAccuracy: " + position.coords.accuracy);
     console.log(position);
-    window.karte = L.map('map').setView([
-      position.coords.latitude, position.coords.longitude
-    ], 13);
+
+    window.karte = L.map('map').setView(latlng, 17);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(window.karte);
 
     // add a marker in the given location, attach some popup content to it and open the popup
-    L.marker([position.coords.latitude, position.coords.longitude]).addTo(window.karte)
-    .bindPopup('You are here ;)')
-    .openPopup();
+    L.marker(latlng).addTo(window.karte).bindPopup('You are here ;)').openPopup();
+
+    // Add radius (accuracy)
+    L.circle(latlng, radius).addTo(window.karte);
   }
 
   function addCurrentPosition(position) {
-    L.marker([position.coords.latitude, position.coords.longitude]).addTo(window.karte)
-    .bindPopup('Now we\'re here!')
+    var radius = position.coords.accuracy / 2;
+    var lat = position.coords.latitude; 
+    var lng = position.coords.longitude;
+    //Testing:
+    lat = parseInt(lat) + (Math.random() - 0.5)/10;
+    lng = parseInt(lng) + (Math.random() - 0.5)/10;
+    var latlng = new L.LatLng(lat,lng);
+
+    window.waypoints.push(latlng);
+    window.polyline = L.polyline(window.waypoints, {color: 'blue'});
+    window.karte.fitBounds(window.polyline.getBounds());
+    window.wpNo++;
+
+    L.marker(latlng).addTo(window.karte)
+    .bindPopup('Waypoint ' + window.wpNo)
     .openPopup();
+
+    // Add radius (accuracy)
+    L.circle(latlng, radius).addTo(window.karte);
+  }
+
+  function joinToPath() {
+    // create a red polyline from an arrays of LatLng points
+    window.polyline = L.polyline(window.waypoints, {color: 'blue'}).addTo(window.karte);
+    
+    // zoom the map to the polyline
+    window.karte.fitBounds(window.polyline.getBounds());
   }
 
   $("#addPosition").click(function () {
     getLocation(addCurrentPosition);
+  });
+
+  $("#joinToPath").click(function () {
+    joinToPath();
   });
 
   getLocation(showPosition);
